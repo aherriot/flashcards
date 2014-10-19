e5a24ccef3cf5919ca45ef41812a04d71d0590e4
Andrew Herriot <agherriot@gmail.com>
2014-10-18 20:39:42 -0700

    Initial yo generated content

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('flashcardsApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
