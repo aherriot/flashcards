6c580746cb918d9449677ae205e2cfbc7e5c4a3f
Andrew Herriot <agherriot@gmail.com>
2014-10-23 18:08:05 -0700

    removing reference to Thing model created by Yeoman.

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -12,8 +12,6 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
 
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
@@ -23,6 +21,5 @@ describe('Controller: MainCtrl', function () {
 
   it('should attach a list of things to the scope', function () {
     $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
   });
 });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,9 +3,6 @@
 angular.module('flashcardsApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }, {
       'title': 'Words',
       'link': '/words'
     }, {
